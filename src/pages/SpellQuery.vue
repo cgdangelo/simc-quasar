9949a06ee1b85a3b42798f18000e703f7efa5914
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-07-13 15:39:31 -0400

    Add azeriteByClass and spellByName premade queries

--- src/pages/SpellQuery.vue
+++ src/pages/SpellQuery.vue
@@ -4,44 +4,100 @@
       <q-card-main>
         <div class="row gutter-md no-wrap">
           <div class="col">
-            <q-select float-label="Data Source" v-model="dataSource" :options="dataSources" />
+            <q-select
+              float-label="Data Source"
+              v-model="dataSource"
+              :options="dataSources"
+            />
           </div>
           <div class="col">
-            <q-select float-label="Filter" v-model="filter" :options="filters" @input="setFilterType" />
+            <q-select
+              float-label="Filter"
+              v-model="filter"
+              :options="filters"
+              @input="setFilterType"
+            />
           </div>
           <div class="col">
-            <q-select float-label=" " v-model="operator" :options="filterType === Number ? numericOperators : stringOperators" />
+            <q-select
+              float-label=" "
+              v-model="operator"
+              :options="filterType === Number ? numericOperators : stringOperators"
+            />
           </div>
           <div class="col">
-            <q-search float-label="Argument" v-model="argument" @keyup.enter="executeQuery" />
+            <q-search
+              float-label="Argument"
+              v-model="argument"
+              @keyup.enter="executeQuery"
+            />
           </div>
           <div class="col-auto">
-            <q-btn-group >
-              <q-btn outline color="primary" label="Run" size="form-label" @click="executeQuery" />
-              <q-btn outline color="primary" label="Premades" size="form-label" @click="showPremades" />
+            <q-btn-group>
+              <q-btn
+                outline
+                color="primary"
+                label="Run"
+                size="form-label"
+                @click="executeQuery"
+              />
+              <q-btn
+                outline
+                color="primary"
+                label="Premades"
+                size="form-label"
+                @click="showPremades"
+              />
             </q-btn-group>
           </div>
         </div>
       </q-card-main>
     </q-card>
 
-    <q-dialog stack-buttons v-model="premadesDialog" title="Premade Queries">
-      <template slot="buttons" slot-scope="props">
-        <q-btn outline label="List all Azerite traits for a class" />
-        <q-btn outline label="List all Azerite traits for a class" />
-        <q-btn outline label="List all Azerite traits for a class" />
-        <q-btn outline label="List all Azerite traits for a class" />
-        <q-btn outline label="List all Azerite traits for a class" />
+    <q-dialog
+      stack-buttons
+      v-model="premadesDialog"
+      title="Premade Queries"
+      class="premades-dialog"
+    >
+      <template
+        slot="buttons"
+        slot-scope="props"
+      >
+        <q-btn
+          outline
+          label="List all Azerite traits for a class"
+          @click="classPrompt(props.ok, azeriteByClass)"
+          class="full-width"
+        />
+        <q-btn
+          outline
+          label="Find a spell by its name"
+          @click="textPrompt(props.ok, 'Enter a spell name.', spellByName)"
+          class="full-width"
+        />
       </template>
     </q-dialog>
 
-    <q-card v-for="(cardDatum, i) in cardData" :key="i" class="query-result q-mt-md">
+    <q-card
+      v-for="(cardDatum, i) in cardData"
+      :key="i"
+      class="query-result q-mt-md"
+    >
       <q-card-title class="bg-primary text-white">
         {{ cardDatum.name }}
-        <span slot="subtitle" v-if="cardDatum.description" class="text-white">{{ cardDatum.description }}</span>
+        <span
+          slot="subtitle"
+          v-if="cardDatum.description"
+          class="text-white"
+        >
+          {{ cardDatum.description }}
+        </span>
       </q-card-title>
       <q-card-separator />
-      <q-card-main><pre>{{ cardDatum.data.join('\n') }}</pre></q-card-main>
+      <q-card-main>
+        <pre>{{ cardDatum.data.join('\n') }}</pre>
+      </q-card-main>
     </q-card>
 
     <q-btn
@@ -57,9 +113,19 @@
 </template>
 
 <script>
-import { exec } from 'child_process'
+import {exec} from 'child_process'
 
-const EOL = require('os').platform() === 'win32' ? '\r\n' : '\n';
+function createSelectChoices (choices) {
+  return choices.map(choice => {
+    if (typeof choice === 'string') {
+      return {$label: choice, value: choice.toLowerCase().replace(' ', '_')}
+    }
+
+    return choice
+  })
+}
+
+const EOL = require('os').platform() === 'win32' ? '\r\n' : '\n'
 
 export default {
   methods: {
@@ -74,17 +140,19 @@ export default {
       this.$q.actionSheet({
         title: 'Premade Queries',
         actions: [
-          { label: 'Find a spell by its id' },
-          { label: 'Find a spell by its name' },
-          { label: 'Find all spells for a class' }
+          {label: 'Find a spell by its id'},
+          {label: 'Find a spell by its name'},
+          {label: 'Find all spells for a class'}
         ]
       })
     },
-    showPremadesDialog () { this.premadesDialog = true },
+    showPremadesDialog () {
+      this.premadesDialog = true
+    },
     executeQuery () {
       const query = `spell_query=${this.dataSource}.${this.filter}${this.operator}${this.argument}`
 
-      exec(`simc "${query}"`, { maxBuffer: 1024 * 1024 }, (err, stdout) => {
+      exec(`simc "${query}"`, {maxBuffer: 1024 * 1024}, (err, stdout) => {
         if (err) {
           this.$q.notify({
             message: `Spell query failed: ${err.message}`,
@@ -105,7 +173,7 @@ export default {
           }
 
           const [, name] = nameLine.match(/Name\s+: (.+) \(id=/m)
-          const cardDatum = { name, data: lines }
+          const cardDatum = {name, data: lines}
 
           // FIXME: This is broken for multi-line descriptions.
           const descriptionLine = lines.find(line => line.startsWith('Description'))
@@ -120,7 +188,57 @@ export default {
         this.cardData = cardData
       })
     },
+    async classPrompt (okFn, choiceFn) {
+      await okFn()
 
+      try {
+        const classChoice = await this.$q.dialog({
+          options: {
+            type: 'radio',
+            model: 'classChoice',
+            items: this.classes
+          },
+          cancel: true,
+          preventClose: true
+        })
+
+        choiceFn(classChoice)
+
+        this.executeQuery()
+      } catch (e) {
+      }
+    },
+    async textPrompt (okFn, promptTitle, choiceFn) {
+      await okFn()
+
+      try {
+        const textChoice = await this.$q.dialog({
+          title: promptTitle,
+          prompt: {
+            model: ''
+          },
+          cancel: true,
+          preventClose: true
+        })
+
+        choiceFn(textChoice)
+
+        this.executeQuery()
+      } catch (e) {
+      }
+    },
+    azeriteByClass (className) {
+      this.dataSource = 'azerite'
+      this.filter = 'class'
+      this.operator = '=='
+      this.argument = className
+    },
+    spellByName (name) {
+      this.dataSource = 'spell'
+      this.filter = 'name'
+      this.operator = '=='
+      this.argument = name.replace(/[-']/, '').replace(' ', '_')
+    },
     setFilterType (value) {
       switch (value) {
         case 'name':
@@ -134,16 +252,18 @@ export default {
   },
   data () {
     return {
-      dataSources: ['Effect', 'Spell'].map(dataSource => ({ label: dataSource, value: dataSource.toLowerCase() })),
-      filters: ['ID', 'Class', 'Name'].map(filter => ({ label: filter, value: filter.toLowerCase() })),
-      numericOperators: ['<', '<=', '==', '>=', '>', '!='].map(operator => ({ label: operator, value: operator })),
-      stringOperators: ['==', '!=', '~', '!~'].map(operator => ({ label: operator, value: operator })),
+      classes: createSelectChoices(['Death Knight', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior']),
+      dataSources: createSelectChoices(['Azerite', 'Effect', 'Spell']),
+      filters: createSelectChoices(['Class', 'ID', 'Name']),
+      numericOperators: createSelectChoices(['<', '<=', '==', '>=', '>', '!=']),
+      stringOperators: createSelectChoices(['==', '!=', '~', '!~']),
       filterType: 'numeric',
       dataSource: 'spell',
       filter: 'class',
       operator: '==',
       argument: 'mage',
       premadesDialog: false,
+      classDialog: false,
       cardData: []
     }
   }
@@ -151,6 +271,9 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.premades-dialog >>> .modal-header
+  text-align: center
+
 .query-results
   margin-top: 5rem
 
