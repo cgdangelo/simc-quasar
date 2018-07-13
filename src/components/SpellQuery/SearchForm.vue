6c009342f362ec399f2d9b92443994b4d02a7117
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-07-13 18:37:12 -0400

    Extract form into separate component

---
+++ src/components/SpellQuery/SearchForm.vue
@@ -0,0 +1,254 @@
+<template>
+  <div>
+    <q-card class="bg-white q-mb-xl">
+      <q-card-main>
+        <div
+          class="row gutter-sm justify-between"
+          v-if="manualMode"
+        >
+          <div class="col-9">
+            <q-input
+              v-model="manualQuery"
+              float-label="Manual Query"
+              prefix="spell_query="
+              class="no-margin"
+              @keyup.enter="executeQuery"
+            />
+          </div>
+          <div class="col">
+            <q-btn-group
+              push
+              class="full-width"
+            >
+              <q-btn
+                push
+                color="primary"
+                label="Run"
+                size="form-label"
+                @click="executeQuery"
+                style="width: 50%"
+              />
+              <q-btn
+                push
+                color="primary"
+                label="Cancel"
+                size="form-label"
+                @click="manualMode = false"
+                style="width: 50%"
+              />
+            </q-btn-group>
+          </div>
+        </div>
+
+        <div
+          class="row gutter-md no-wrap"
+          v-else
+        >
+          <div class="col">
+            <q-select
+              float-label="Data Source"
+              v-model="dataSource"
+              :options="dataSources"
+            />
+          </div>
+          <div class="col">
+            <q-select
+              float-label="Filter"
+              v-model="filter"
+              :options="filters"
+              @input="setFilterType"
+            />
+          </div>
+          <div class="col">
+            <q-select
+              float-label=" "
+              v-model="operator"
+              :options="filterType === Number ? numericOperators : stringOperators"
+            />
+          </div>
+          <div class="col">
+            <q-search
+              float-label="Argument"
+              v-model="argument"
+              @keyup.enter="executeQuery"
+            />
+          </div>
+          <div class="col-auto">
+            <q-btn-group push>
+              <q-btn
+                push
+                color="primary"
+                label="Run"
+                size="form-label"
+                @click="executeQuery"
+              />
+              <q-btn
+                push
+                color="primary"
+                label="Premades"
+                size="form-label"
+                @click="showPremades"
+              />
+              <q-btn
+                push
+                color="primary"
+                label="Manual"
+                size="form-label"
+                @click="manualMode = true"
+              />
+            </q-btn-group>
+          </div>
+        </div>
+      </q-card-main>
+    </q-card>
+
+    <q-dialog
+      stack-buttons
+      v-model="premadesDialog"
+      title="Premade Queries"
+      class="premades-dialog"
+    >
+      <template
+        slot="buttons"
+        slot-scope="props"
+      >
+        <q-btn
+          v-for="(query, i) in premadeQueries"
+          :key="i"
+          :label="query.label"
+          @click="query.handler(props.ok)"
+          flat
+          class="full-width"
+        />
+      </template>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+function createSelectChoices (choices) {
+  return choices.map(choice => {
+    if (typeof choice === 'string') {
+      return {label: choice, value: choice.toLowerCase().replace(' ', '_')}
+    }
+
+    return choice
+  })
+}
+
+export default {
+  name: 'SearchForm',
+  data () {
+    return {
+      classes: createSelectChoices(['Death Knight', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior']),
+      dataSources: createSelectChoices(['Azerite', 'Effect', 'Spell']),
+      filters: createSelectChoices(['Class', 'ID', 'Name']),
+      numericOperators: createSelectChoices(['<', '<=', '==', '>=', '>', '!=']),
+      stringOperators: createSelectChoices(['==', '!=', '~', '!~']),
+      filterType: 'numeric',
+      dataSource: 'spell',
+      filter: 'class',
+      operator: '==',
+      argument: 'mage',
+      premadesDialog: false,
+      premadeQueries: [
+        {label: 'Find all Azerite traits for a class', handler: this.azeriteByClass},
+        {label: 'Find a spell by its name', handler: this.spellByName}
+      ],
+      classDialog: false,
+      manualMode: false,
+      manualQuery: null
+    }
+  },
+  methods: {
+    showPremades () {
+      if (this.$q.platform.is.mobile) {
+        this.showPremadesSheet()
+      } else {
+        this.showPremadesDialog()
+      }
+    },
+    showPremadesSheet () {
+      this.$q.actionSheet({
+        title: 'Premade Queries',
+        actions: this.premadeQueries
+      })
+    },
+    showPremadesDialog () {
+      this.premadesDialog = true
+    },
+    async classPrompt () {
+      return this.$q.dialog({
+        options: {
+          type: 'radio',
+          model: '',
+          items: this.classes
+        },
+        cancel: true,
+        preventClose: true
+      })
+    },
+    async textPrompt (promptTitle, promptMessage) {
+      return this.$q.dialog({
+        title: promptTitle,
+        message: promptMessage,
+        prompt: {
+          model: ''
+        },
+        cancel: true,
+        preventClose: true
+      })
+    },
+    async azeriteByClass (okFn) {
+      await okFn()
+
+      try {
+        const className = await this.classPrompt()
+
+        this.dataSource = 'azerite'
+        this.filter = 'class'
+        this.operator = '=='
+        this.argument = className
+
+        this.executeQuery()
+      } catch (e) {
+      }
+    },
+    async spellByName (okFn) {
+      await okFn()
+
+      try {
+        const name = await this.textPrompt('Spell by name', 'Enter a spell name. You can enter a literal or pre-formatted name, e.g. Arcane Missiles or arcane_missiles.')
+
+        this.dataSource = 'spell'
+        this.filter = 'name'
+        this.operator = '=='
+        this.argument = name.replace(/[-']/, '').replace(' ', '_')
+
+        this.executeQuery()
+      } catch (e) {
+      }
+    },
+    setFilterType (value) {
+      switch (value) {
+        case 'name':
+          this.filterType = String
+          break
+        case 'id':
+          this.filterType = Number
+          break
+      }
+    },
+    executeQuery () {
+      const query = `spell_query=${this.manualMode ? this.manualQuery : `${this.dataSource}.${this.filter}${this.operator}${this.argument}`}`
+
+      this.$emit('execute-query', query)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.premades-dialog >>> .modal-header
+  text-align: center
+</style>
--- src/pages/SpellQuery.vue
+++ src/pages/SpellQuery.vue
@@ -1,128 +1,6 @@
 <template>
   <q-page padding>
-    <q-card class="bg-white q-mb-xl">
-      <q-card-main>
-        <div
-          class="row gutter-sm justify-between"
-          v-if="manualMode"
-        >
-          <div class="col-9">
-            <q-input
-              v-model="manualQuery"
-              float-label="Manual Query"
-              prefix="spell_query="
-              class="no-margin"
-              @keyup.enter="executeQuery"
-            />
-          </div>
-          <div class="col">
-            <q-btn-group class="full-width">
-              <q-btn
-                outline
-                color="primary"
-                label="Run"
-                size="form-label"
-                @click="executeQuery"
-                style="width: 50%"
-              />
-              <q-btn
-                outline
-                color="primary"
-                label="Cancel"
-                size="form-label"
-                @click="manualMode = false"
-                style="width: 50%"
-              />
-            </q-btn-group>
-          </div>
-        </div>
-
-        <div
-          class="row gutter-md no-wrap"
-          v-else
-        >
-          <div class="col">
-            <q-select
-              float-label="Data Source"
-              v-model="dataSource"
-              :options="dataSources"
-            />
-          </div>
-          <div class="col">
-            <q-select
-              float-label="Filter"
-              v-model="filter"
-              :options="filters"
-              @input="setFilterType"
-            />
-          </div>
-          <div class="col">
-            <q-select
-              float-label=" "
-              v-model="operator"
-              :options="filterType === Number ? numericOperators : stringOperators"
-            />
-          </div>
-          <div class="col">
-            <q-search
-              float-label="Argument"
-              v-model="argument"
-              @keyup.enter="executeQuery"
-            />
-          </div>
-          <div class="col-auto">
-            <q-btn-group>
-              <q-btn
-                outline
-                color="primary"
-                label="Run"
-                size="form-label"
-                @click="executeQuery"
-              />
-              <q-btn
-                outline
-                color="primary"
-                label="Premades"
-                size="form-label"
-                @click="showPremades"
-              />
-              <q-btn
-                outline
-                color="primary"
-                label="Manual"
-                size="form-label"
-                @click="manualMode = true"
-              />
-            </q-btn-group>
-          </div>
-        </div>
-      </q-card-main>
-    </q-card>
-
-    <q-dialog
-      stack-buttons
-      v-model="premadesDialog"
-      title="Premade Queries"
-      class="premades-dialog"
-    >
-      <template
-        slot="buttons"
-        slot-scope="props"
-      >
-        <q-btn
-          outline
-          label="List all Azerite traits for a class"
-          @click="classPrompt(props.ok, azeriteByClass)"
-          class="full-width"
-        />
-        <q-btn
-          outline
-          label="Find a spell by its name"
-          @click="textPrompt(props.ok, 'Enter a spell name.', spellByName)"
-          class="full-width"
-        />
-      </template>
-    </q-dialog>
+    <SearchForm @execute-query="executeQuery" />
 
     <p
       v-if="cardData.length > 0"
@@ -166,44 +44,14 @@
 
 <script>
 import {exec} from 'child_process'
-
-function createSelectChoices (choices) {
-  return choices.map(choice => {
-    if (typeof choice === 'string') {
-      return {label: choice, value: choice.toLowerCase().replace(' ', '_')}
-    }
-
-    return choice
-  })
-}
+import SearchForm from '../components/SpellQuery/SearchForm'
 
 const EOL = require('os').platform() === 'win32' ? '\r\n' : '\n'
 
 export default {
+  components: {SearchForm},
   methods: {
-    showPremades () {
-      if (this.$q.platform.is.mobile) {
-        this.showPremadesSheet()
-      } else {
-        this.showPremadesDialog()
-      }
-    },
-    showPremadesSheet () {
-      this.$q.actionSheet({
-        title: 'Premade Queries',
-        actions: [
-          {label: 'Find a spell by its id'},
-          {label: 'Find a spell by its name'},
-          {label: 'Find all spells for a class'}
-        ]
-      })
-    },
-    showPremadesDialog () {
-      this.premadesDialog = true
-    },
-    executeQuery () {
-      const query = `spell_query=${this.manualMode ? this.manualQuery : `${this.dataSource}.${this.filter}${this.operator}${this.argument}`}`
-
+    executeQuery (query) {
       exec(`simc "${query}"`, {maxBuffer: 1024 * 1024}, (err, stdout) => {
         if (err) {
           this.$q.notify({
@@ -239,85 +87,10 @@ export default {
 
         this.cardData = cardData
       })
-    },
-    async classPrompt (okFn, choiceFn) {
-      await okFn()
-
-      try {
-        const classChoice = await this.$q.dialog({
-          options: {
-            type: 'radio',
-            model: '',
-            items: this.classes
-          },
-          cancel: true,
-          preventClose: true
-        })
-
-        choiceFn(classChoice)
-
-        this.executeQuery()
-      } catch (e) {
-      }
-    },
-    async textPrompt (okFn, promptTitle, choiceFn) {
-      await okFn()
-
-      try {
-        const textChoice = await this.$q.dialog({
-          title: promptTitle,
-          prompt: {
-            model: ''
-          },
-          cancel: true,
-          preventClose: true
-        })
-
-        choiceFn(textChoice)
-
-        this.executeQuery()
-      } catch (e) {
-      }
-    },
-    azeriteByClass (className) {
-      this.dataSource = 'azerite'
-      this.filter = 'class'
-      this.operator = '=='
-      this.argument = className
-    },
-    spellByName (name) {
-      this.dataSource = 'spell'
-      this.filter = 'name'
-      this.operator = '=='
-      this.argument = name.replace(/[-']/, '').replace(' ', '_')
-    },
-    setFilterType (value) {
-      switch (value) {
-        case 'name':
-          this.filterType = String
-          break
-        case 'id':
-          this.filterType = Number
-          break
-      }
     }
   },
   data () {
     return {
-      classes: createSelectChoices(['Death Knight', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior']),
-      dataSources: createSelectChoices(['Azerite', 'Effect', 'Spell']),
-      filters: createSelectChoices(['Class', 'ID', 'Name']),
-      numericOperators: createSelectChoices(['<', '<=', '==', '>=', '>', '!=']),
-      stringOperators: createSelectChoices(['==', '!=', '~', '!~']),
-      filterType: 'numeric',
-      dataSource: 'spell',
-      filter: 'class',
-      operator: '==',
-      argument: 'mage',
-      premadesDialog: false,
-      classDialog: false,
-      manualMode: false,
-      manualQuery: null,
       cardData: []
     }
   }
@@ -325,9 +98,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.premades-dialog >>> .modal-header
-  text-align: center
-
 .query-results
   margin-top: 5rem
 
